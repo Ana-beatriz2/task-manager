00d17b05b8d074cfee616e177d672f2098eaabb2
Ana-beatriz2 <caldas.ana9@gmail.com>
2023-12-29 19:05:41 -0300

    build: inicialização do projeto

---
+++ api/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/", routes)
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
+
